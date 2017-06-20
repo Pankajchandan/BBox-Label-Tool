b8375dc86849b1d6115cb602e6fb141b90d5850b
Pankajchandan <pankaj.mest@gmail.com>
2017-06-19 23:45:32 -0700

    changed

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-#-------------------------------------------------------------------------------
-# Name:        Object bounding box label tool
-# Purpose:     Label object bboxes for ImageNet Detection data
-# Author:      Qiushi
-# Created:     06/06/2014
 
-#
-#-------------------------------------------------------------------------------
+# coding: utf-8
+
+# In[5]:
+
 from __future__ import division
-from Tkinter import *
-import tkMessageBox
+from tkinter import *
+from tkinter import messagebox as tkMessageBox
 from PIL import Image, ImageTk
 import os
 import glob
 import random
 
+
+# In[7]:
+
 # colors for the bboxes
 COLORS = ['red', 'blue', 'yellow', 'pink', 'cyan', 'green', 'black']
 # image sizes for the examples
-SIZE = 256, 256
+SIZE = 640, 640
 
 class LabelTool():
     def __init__(self, master):
@@ -129,11 +129,12 @@ class LabelTool():
 ##        if not os.path.isdir(s):
 ##            tkMessageBox.showerror("Error!", message = "The specified dir doesn't exist!")
 ##            return
+
         # get image list
         self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
         self.imageList = glob.glob(os.path.join(self.imageDir, '*.JPEG'))
         if len(self.imageList) == 0:
-            print 'No .JPEG images found in the specified dir!'
+            print('No .JPEG images found in the specified dir!')
             return
 
         # default to the 1st image in the collection
@@ -164,7 +165,7 @@ class LabelTool():
             self.egLabels[i].config(image = self.egList[-1], width = SIZE[0], height = SIZE[1])
 
         self.loadImage()
-        print '%d images loaded from %s' %(self.total, s)
+        print('%d images loaded from %s' %(self.total, s))
 
     def loadImage(self):
         # load image
@@ -190,10 +191,7 @@ class LabelTool():
                     tmp = [int(t.strip()) for t in line.split()]
 ##                    print tmp
                     self.bboxList.append(tuple(tmp))
-                    tmpId = self.mainPanel.create_rectangle(tmp[0], tmp[1], \
-                                                            tmp[2], tmp[3], \
-                                                            width = 2, \
-                                                            outline = COLORS[(len(self.bboxList)-1) % len(COLORS)])
+                    tmpId = self.mainPanel.create_rectangle(tmp[0], tmp[1],                                                             tmp[2], tmp[3],                                                             width = 2,                                                             outline = COLORS[(len(self.bboxList)-1) % len(COLORS)])
                     self.bboxIdList.append(tmpId)
                     self.listbox.insert(END, '(%d, %d) -> (%d, %d)' %(tmp[0], tmp[1], tmp[2], tmp[3]))
                     self.listbox.itemconfig(len(self.bboxIdList) - 1, fg = COLORS[(len(self.bboxIdList) - 1) % len(COLORS)])
@@ -203,7 +201,7 @@ class LabelTool():
             f.write('%d\n' %len(self.bboxList))
             for bbox in self.bboxList:
                 f.write(' '.join(map(str, bbox)) + '\n')
-        print 'Image No. %d saved' %(self.cur)
+        print ('Image No. %d saved' %(self.cur))
 
 
     def mouseClick(self, event):
@@ -231,10 +229,7 @@ class LabelTool():
         if 1 == self.STATE['click']:
             if self.bboxId:
                 self.mainPanel.delete(self.bboxId)
-            self.bboxId = self.mainPanel.create_rectangle(self.STATE['x'], self.STATE['y'], \
-                                                            event.x, event.y, \
-                                                            width = 2, \
-                                                            outline = COLORS[len(self.bboxList) % len(COLORS)])
+            self.bboxId = self.mainPanel.create_rectangle(self.STATE['x'], self.STATE['y'],                                                             event.x, event.y,                                                             width = 2,                                                             outline = COLORS[len(self.bboxList) % len(COLORS)])
 
     def cancelBBox(self, event):
         if 1 == self.STATE['click']:
@@ -291,3 +286,9 @@ if __name__ == '__main__':
     tool = LabelTool(root)
     root.resizable(width =  True, height = True)
     root.mainloop()
+
+
+# In[ ]:
+
+
+
